d7e9489f2fc48ddba33c2abbb7d34487d7611e94
Abdelrahman Eid <eida0556@gmail.com>
2026-04-26 11:53:01 +0300

    Delete dist directory

--- dist/common/enum/post.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.availabilityEnum = void 0;
-var availabilityEnum;
-(function (availabilityEnum) {
-    availabilityEnum[availabilityEnum["PUBLIC"] = 0] = "PUBLIC";
-    availabilityEnum[availabilityEnum["ONLYFRIENDS"] = 1] = "ONLYFRIENDS";
-    availabilityEnum[availabilityEnum["PRIVATE"] = 2] = "PRIVATE";
-})(availabilityEnum || (exports.availabilityEnum = availabilityEnum = {}));
--- dist/common/response/success.response.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.successResponse = void 0;
-const successResponse = ({ res, message = "Success", data, status = 200 }) => {
-    return res.status(status).json({ message, data });
-};
-exports.successResponse = successResponse;
